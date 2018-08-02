42a7d4ebb165524d8e1907659dff959bd32d809f
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2018-08-02 22:19:04 +0530

    regions

---
+++ src/app/regions/regions.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-regions",
+  templateUrl: "./regions.component.html",
+  styleUrls: ["./regions.component.css"]
+})
+export class RegionsComponent implements OnInit {
+  regions: string[];
+  constructor() {}
+
+  ngOnInit() {
+    this.regions = ["americas", "europe", "africa", "asia", "oceania"];
+  }
+
+  getImage = region => `../assets/regions/${region}.svg`;
+  getAlt = region => `region ${region}`;
+}
